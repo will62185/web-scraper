54d058d9d940a566a4365ad5f8d81e0aa35c9da8
Will <will62185@gmail.com>
2019-08-05 19:27:18 -0400

    Reswizzle script and comment code

--- web-scrape.py
+++ web-scrape.py
@@ -1,14 +1,22 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as BS
 import requests
 
-urls = ["https://www.subaruofmiami.com/promotions/service/index.htm","  https://www.lehmansubaru.com/promotions/service/index.htm"] 
+# List of urls to scrape
+urls = [
+    "https://www.subaruofmiami.com/promotions/service/index.htm",
+    "https://www.lehmansubaru.com/promotions/service/index.htm",
+       ] 
 
-#scrape elements
+# Loop through the urls
 for url in urls:
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    print(f"Visit this link for more information: {url}")
+    soup = BS(response.content, "html.parser")
+    
+    # The information was burried in a div
+    html_tag = soup.find_all("div", class_="promo-title text-center h1")
 
-    h1 = soup.find_all("div", class_="promo-title text-center h1")
-    for tag in h1:
-        print(tag.text)
+    # The find_all method places the results in a list, so this needed to be looped through
+    # to get just the text using the .text method.
+    for tag in html_tag: 
+        print(tag.text.strip())
+    print(f"Visit this link for more information: {url}")
